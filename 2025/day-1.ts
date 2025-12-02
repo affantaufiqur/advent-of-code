f7a81b7307b620724f7e5caf985e907f132b3d60
affantaufiqur <affantaufiqur@gmail.com>
2025-12-02 11:21:35 +0700

    [2025]: day 1

---
+++ 2025/day-1.ts
@@ -0,0 +1,40 @@
+const file = Bun.file("./day-1.txt");
+const input = (await file.text()) as string;
+
+let count = 50;
+
+const dials = input
+  .split("\n")
+  .map((f) => f.trim())
+  .filter((f) => f.length > 0);
+
+let atZero = 0;
+
+function dialFn(value: number, direction: "L" | "R") {
+  if (direction === "L") {
+    return (count - value) % 100;
+  }
+
+  return (count + value) % 100;
+}
+
+for (let i = 0; i < dials.length; i++) {
+  if (count === 0) atZero++;
+
+  const dial = dials[i];
+  const operator = dial.charAt(0);
+
+  const number = parseInt(dial.slice(1), 10);
+
+  if (operator === "L") {
+    count = dialFn(number, "L");
+    continue;
+  }
+
+  if (operator === "R") {
+    count = dialFn(number, "R");
+    continue;
+  }
+}
+
+console.log(atZero);
