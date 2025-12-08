0bb7a51aec3caf95cb37aba562068f5bfdd9bc1d
affantaufiqur <affantaufiqur@gmail.com>
2025-12-08 17:57:35 +0700

    [2025]: day 3 part 2

--- 2025/day-3.ts
+++ 2025/day-3.ts
@@ -9,28 +9,61 @@ const input = read
 
 // console.log("input is", input);
 
-let maxArr: number[] = [];
-
-for (let i = 0; i < input.length; i++) {
-  const numbers = input[i]; // full number string
-  let max = 0;
-  // console.log("numer is", numbers);
-  for (let j = 0; j < numbers.length; j++) {
-    const digit = numbers[j]; // individual digit
-    for (let k = j + 1; k < numbers.length; k++) {
-      const adj = numbers[k];
-      // console.log("digit", digit, "adj", adj);
-      const joined = [digit, adj].join("");
-      max = Math.max(max, parseInt(joined, 10));
+function part1(input: string[]) {
+  let maxArr: number[] = [];
+  for (let i = 0; i < input.length; i++) {
+    const numbers = input[i]; // full number string
+    let max = 0;
+    // console.log("numer is", numbers);
+    for (let j = 0; j < numbers.length; j++) {
+      const digit = numbers[j]; // individual digit
+      for (let k = j + 1; k < numbers.length; k++) {
+        const adj = numbers[k];
+        // console.log("digit", digit, "adj", adj);
+        const joined = [digit, adj].join("");
+        max = Math.max(max, parseInt(joined, 10));
+      }
     }
+
+    maxArr.push(max);
+  }
+}
+
+function part2(input: string[], total = 12) {
+  const results: number[] = [];
+  for (let i = 0; i < input.length; i++) {
+    const sequence = input[i];
+
+    const len = sequence.length;
+
+    let n = total - 1;
+    let start = 0;
+    const digits: number[] = [];
+
+    while (n >= 0) {
+      for (let i = start; i < len - n; i++) {
+        if (sequence[i] > sequence[start]) {
+          start = i;
+        }
+      }
+
+      digits.push(+sequence[start]);
+      start += 1;
+      n--;
+    }
+
+    results.push(parseInt(digits.join(""), 10));
   }
 
-  maxArr.push(max);
+  return results;
 }
 
 function sum(numbers: number[]) {
   return numbers.reduce((acc, curr) => acc + curr, 0);
 }
 
-console.log("maxArr", maxArr);
-console.log("sum", sum(maxArr));
+const part2Results = part2(input);
+const sumOfPart2 = sum(part2Results);
+
+console.log("part 2", part2Results);
+console.log("sum of part 2", sumOfPart2);
